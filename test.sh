45f3b7c0194a3bcd3e8755f6fccd84a6e93c22c3
Antoine Pernot <antoinepernot@mailoo.org>
2015-05-27 15:52:59 +0200

    Début de afinger.

--- test.sh
+++
@@ -1,3 +0,0 @@
-nbusers=$(sqlite3 -column database.db "SELECT login FROM users;" | wc -l)
-    users=$(sqlite3 -column database.db "SELECT login FROM users;")
-    user=$(whiptail --title "rvsh - Modification d'utilisateurs" --menu "Choisissez les comptes utilisateurs à modifier : " 20 78 ${nbhosts} ${hosts} 3>&1 1>&2 2>&3)
